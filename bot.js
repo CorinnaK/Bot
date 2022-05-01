21c4820c498b3e9de34aac9fe596641dcb666891
Corinna Kelly <corinna1@UAlberta.ca>
2022-05-01 11:03:57 -0600

    Added welcome channel

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+const generateImage = require("./generateImage");
 
 const Discord = require("discord.js");
 const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_MESSAGES"],
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
 client.on("ready", () => {
@@ -10,9 +11,19 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (msg) => {
-  if (msg.content === "hi") {
+  if (msg.content.toLowerCase() === "hi") {
     msg.reply("Hello fellow Person");
   }
+});
+
+const welcomeChannelId = "969749401759256636";
+
+client.on("guildMemberAdd", async (member) => {
+  const img = await generateImage(member);
+  member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}> Welcome to the Server!`,
+    files: [img],
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
